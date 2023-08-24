c2691a02fa4e7d6bb26070f86b9d9bda008f19da
Alexandre <alexsales001@hotmail.com>
2023-08-24 18:26:12 -0300

    Linters fix

--- decode.rb
+++ decode.rb
@@ -1,30 +1,31 @@
 # method to translate one character from morse
 class Decode
-  Decode_letter_hash = {
+  DECODE_LETTER_HASH = {
     '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
     '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
     '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V',
     '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z'
   }.freeze
   def decode_char(char)
-    return Decode_letter_hash[char] if Decode_letter_hash.key?(char)
+    return DECODE_LETTER_HASH[char] if DECODE_LETTER_HASH.key?(char)
+
     'Character not found'
   end
+
   def decode_word(word)
     sentence = word.split
     sentence2 = sentence.map { |char| decode_char(char) }
-    sentence3 = sentence2.join
-    return sentence3
+    sentence2.join
   end
+
   def decode_msj(msj)
     sentence = msj.split('   ')
     sentence2 = sentence.map { |word| decode_word(word) }
-    sentence3 = sentence2.join(' ')
-    return sentence3
+    sentence2.join(' ')
   end
 end
 test = Decode.new
 puts test.decode_char('.-')
 puts test.decode_word('-- -.--')
 puts test.decode_msj('-- -.--   -. .- -- .')
-puts test.decode_msj(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+puts test.decode_msj('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
